165fa367202322ca005022ae92bfb487b507b629
Wammouial <williamdutupem@gmail.com>
2019-05-25 03:19:36 +0200

    test

--- board.py
+++ board.py
@@ -258,9 +258,9 @@ class IA(object):
 		
 	def choice(self):
 		self.getWhereMore()
-		print(self.board.strWithCells(self.available))
+		logger.info(self.board.strWithCells(self.available))
 		self.getBiggest()
-		print(self.board.strWithCells(self.available))
+		logger.info(self.board.strWithCells(self.available))
 		
 		return self.alea()
 	
--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from random import choice
 
 from board import Position, Cell, Path, Board, IA
 from player import Player
+from logs import logger
 
 LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 NUMBERS = list(range(1, 11))
@@ -37,24 +38,30 @@ class Game(object):
 
 	def turn(self, rcvd=None, first=False, illegalUs=False, illegalOther=False, other=False):
 		if illegalUs: # Si on a mal joué au tour d'avant
+			logger.info("Mal joué avant")
 			cell = self.removeLastPrevious()
 			self.board.updateCell(cell.position, None)
 			return  # Enlever le coup qu'on a fait avant
 
 		if illegalOther:
+			logger.info("Ennemi mechant")
 			return
 
 		if other and rcvd is not None:  # Update la position de l'adversaire
-			self.board.updateCell(self.parseRcvdPosition(rcvd), self.player2)
+			logger.info("Ennemi a joué :)")
+			pos = self.parseRcvdPosition(rcvd)
+			self.board.updateCell(pos, self.player2)
+			self.addPrevious(self.board[pos])
 			return
 
 		if first and rcvd is not None:
+			logger.info("First")
 			self.board.updateBoard(rcvd)
 
 		if rcvd is not None:
 			ia = IA(self.board, self.previous, self.player1)
 			choosen = ia.choice()
-
+			logger.info(choosen)
 			self.board.updateCell(choosen.position, self.player1)
 			self.addPrevious(choosen)
 
